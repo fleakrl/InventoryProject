cdc64759e568f0c8b3e3fc588f5e12fe0ebb12d6
Rebecca Fleak <fleakrl@gmail.com>
2017-11-09 19:53:50 -0600

    started Book class

--- src/main/java/com/example/demo/books/Book.java
+++ src/main/java/com/example/demo/books/Book.java
@@ -1,4 +1,17 @@
 package com.example.demo.books;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Book {
+
+    private Long bookId;
+    private String title;
+    private String author;
+    private String publisher;
+    private int yearPublished;
+    private String ISBN;
+
 }
