d81b57cb7015e216cb7e68f53536238c98d6e3ae
Rebecca Fleak <fleakrl@gmail.com>
2017-11-21 20:08:14 -0600

    added javadoc comments

--- src/main/java/com/example/demo/movies/controller/MoviesController.java
+++ src/main/java/com/example/demo/movies/controller/MoviesController.java
@@ -17,11 +17,21 @@ public class MoviesController {
 
     private final MoviesDelegate moviesDelegate;
 
+    /**
+     * On GET requests to /movies return a list of all movies in the movies table
+     * @return LIST<MoviesDto> of the movies in the movies table
+     */
     @RequestMapping(method = RequestMethod.GET, path = "/movies")
     public List<MoviesDto> getAllMovies(){
         return moviesDelegate.findAllMovies();
     }
 
+
+    /**
+     * On POST requests to /movies add a movie to the movies table
+     * @param movieToAdd JSON Request body data that gets mapped to a MoviesDto object
+     * @return MovieDto object of the movie data added to the movies table
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/movies")
     public MoviesDto addMovie(@RequestBody MoviesDto movieToAdd){
         return moviesDelegate.addMovie(movieToAdd);
